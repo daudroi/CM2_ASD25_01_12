3feb4ae9272fb9f3d0b6db694c35789b92b6a1a9
Indhirayc <indhirayuantikac1176@gmail.com>
2025-06-03 09:47:41 +0700

    Membuat menu 1

--- CM2_ASD25_01_12/DLL01_12.java
+++ CM2_ASD25_01_12/DLL01_12.java
@@ -4,4 +4,15 @@ public class DLL01_12 {
     Kendaraan01_12 head, tail;
     int size = 0;
     
+    public void tambahData(String platNo, String tipe, String merk) {
+        Kendaraan01_12 newNode = new Kendaraan01_12(tail, platNo, tipe, merk, head);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
 }
--- CM2_ASD25_01_12/MainAntrian01_12.java
+++ CM2_ASD25_01_12/MainAntrian01_12.java
@@ -3,6 +3,31 @@ package CM2_ASD25_01_12;
 import java.util.Scanner;
 public class MainAntrian01_12{
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        DLL01_12 dll = new DLL01_12();
+        int pilih;
+
+        do { 
+            System.out.println("===================");
+            System.out.println("     Pilih Menu    ");
+            System.out.println("===================");
+            System.out.println("1. Tambah Data");
+            System.out.print("Pilih Menu : ");
+            pilih = sc.nextInt();
+            sc.nextLine();
+
+            switch (pilih) {
+                case 1 -> {
+                    System.out.print("Masukan plat nomor : ");
+                    String platNo = sc.nextLine();
+                    System.out.print("Masukan tipe kendaraan : ");
+                    String tipe = sc.nextLine();
+                    System.out.print("Masukan merk : ");
+                    String merk = sc.nextLine();
+                    dll.tambahData(platNo, tipe, merk);
+                }
+                default -> {}
+            }
+        } while (true);
     }
 }
