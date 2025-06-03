d2b2784921d8db766d8ff376e4e80e61ae75ee95
Indhirayc <indhirayuantikac1176@gmail.com>
2025-06-03 12:36:22 +0700

    Update DLLTransaksi01_12.java
    Sorting

--- CM2_ASD25_01_12/DLLTransaksi01_12.java
+++ CM2_ASD25_01_12/DLLTransaksi01_12.java
@@ -14,12 +14,41 @@ public class DLLTransaksi01_12 {
         }
     }
 
-//     public void tampilTransaksi() {
-//         TransaksiPengisian01_12 tmp = head;
-//         System.out.printf("%-15s %-15s %-15s %-10s %-10s %-10s\n", "Plat", "Jenis", "Merk", "BBM", "Liter", "Total");
-//         while (tmp != null) {
-//             tmp.tampilTransaksi();
-//             tmp = tmp.next;
-//         }
-//     }
+    public void tampilTransaksi() {
+        TransaksiPengisian01_12 tmp = head;
+        System.out.printf("%-15s %-15s %-15s %-10s %-10s %-10s\n", "Plat", "Jenis", "Merk", "BBM", "Liter", "Total");
+        while (tmp != null) {
+            tmp.tampilTransaksi();
+            tmp = tmp.next;
+        }
+    }
+
+    public void sortByPlatNomor() {
+        if (head == null || head.next == null) return;
+        TransaksiPengisian01_12 current = head;
+        while (current != null) {
+            TransaksiPengisian01_12 index = current.next;
+            while (index != null) {
+                if (current.kendaraan.platNo.compareTo(index.kendaraan.platNo) > 0) {
+                    Kendaraan01_12 tempK = current.kendaraan;
+                    BBM01_12 tempBBM = current.bbm;
+                    double tempLiter = current.liter;
+                    double tempTotal = current.totalBayar;
+
+                    current.kendaraan = index.kendaraan;
+                    current.bbm = index.bbm;
+                    current.liter = index.liter;
+                    current.totalBayar = index.totalBayar;
+
+                    index.kendaraan = tempK;
+                    index.bbm = tempBBM;
+                    index.liter = tempLiter;
+                    index.totalBayar = tempTotal;
+                }
+                index = index.next;
+            }
+            current = current.next;
+        }
+        System.out.println(">> Riwayat transaksi berhasil diurutkan berdasarkan plat nomor.");
+    }
 }
